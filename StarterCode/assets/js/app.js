9e4c7bd9705795b673cc7c34ffbcbb6f3223f7c4
Alexandra Zapuchlak <azapuchlak@gmail.com>
2021-06-16 23:03:54 -0500

    set up svg/chart dimensions and margins

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -3,3 +3,28 @@ console.log("app.js loaded");
 
 //---------------------------------------------------------------------------//
 
+//Step 1 : Set up Chart
+//define width and height of svg area
+var svgWidth = 700;
+var svgHeight = 500;
+
+//define margins
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 50
+};
+
+//define width and height for chart
+var width = chartWidth - margin.left - margin.right;
+var height = chartHeight - margin.top - margin.bottom;
+
+//Step 2: Create SVG Wrapper
+
+
+
+
+//Step 3: Import Data from data.csv file
+
+
